27f0ad91a70193d9585af544e281ec3984620121
Brian Smith <brian.bubbam@gmail.com>
2021-05-20 13:48:19 -0500

    commit tree 8e536de3584c114dc8081308920a04acc301eecf

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+function findMatching(drivers, passingDrivers) {
+   
+   const newDrivers = drivers.filter(newDriver => newDriver.toUpperCase() === passingDrivers.toUpperCase()) 
+        return newDrivers; 
+}
+
+function fuzzyMatch(driversArray, passingDrivers) {
+
+    return driversArray.filter(newDriver => newDriver.toUpperCase().indexOf(passingDrivers.toUpperCase()) === 0 )
+}
+
+function matchName(driverArray, string) {
+    return driverArray.filter(driver => driver.name === string )
+
+}
+
+//whose name property matches the provided string argument.
+
